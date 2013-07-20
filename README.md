73fc904f19cd2a4a40f1f38509a1a6a91fea3c69
umberto1978 <ur.tecnotop@gmail.com>
2013-07-20 14:01:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+SpeedWizz_N2_Kernel---GT-N7100
+==============================
+
+New project for international version of the Samsung Galaxy Note 2 GT N7100 device
+
+The actual source upload consist:
+
+- The source code jb_update2 released from samsung for N7100
+
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Jul 20 09:28:20 CEST 2013"
+#define LINUX_COMPILE_BY "umberto1978"
+#define LINUX_COMPILE_HOST "SpeedWizz-N2-jb"
+#define LINUX_COMPILER "gcc version 4.6.3 20111201 (Linaro GCC 4.6-2011.12) "
