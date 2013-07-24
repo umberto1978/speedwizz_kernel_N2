dc1de4107700a134ced85d3aeda0ed65d4510626
umberto1978 <ur.tecnotop@gmail.com>
2013-07-24 06:46:06 +0200

    added proper kernel version

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 SMP PREEMPT Tue Jul 23 19:21:19 CEST 2013"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Jul 24 00:00:47 CEST 2013"
 #define LINUX_COMPILE_BY "umberto1978"
-#define LINUX_COMPILE_HOST "SpeedWizz-N2-jb"
+#define LINUX_COMPILE_HOST "SpeedWizz-N2-v1.0.0"
 #define LINUX_COMPILER "gcc version 4.6.3 20111201 (Linaro GCC 4.6-2011.12) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.34"
+#define UTS_RELEASE "3.0.34-gb2db637-dirty"
